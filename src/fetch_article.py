46ed4966347cb3ce067312d93132e2d5dfafbb2b
Pandicon <70060103+Pandicon@users.noreply.github.com>
2025-10-11 13:46:02 +0100

    Add logic for fetching articles and extracting key information

---
+++ src/fetch_article.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+import json
+import trafilatura
+import typing
+
+@dataclass
+class ArticleMetadata:
+    title: str
+    text: str
+    hostname: str
+    def __init__(self, title: str, text: str, hostname: typing.Optional[str]):
+        self.title = title
+        self.text = text
+        self.hostname = hostname if not hostname == None else "unknown"
+
+def fetch_article(url: str) -> typing.Optional[str]:
+    downloaded_page = trafilatura.fetch_url(url)
+    return downloaded_page
+
+def extract_article_metadata(webpage: str) -> typing.Optional[ArticleMetadata]:
+    extracted_raw = trafilatura.extract(webpage, output_format="json", with_metadata=True)
+    if extracted_raw == None:
+        return None
+    extracted_data = json.loads(extracted_raw)
+    return ArticleMetadata(extracted_data.get("title"), extracted_data.get("text"), extracted_data.get("hostname"))
